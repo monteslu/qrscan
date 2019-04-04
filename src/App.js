f3c4cf2819621e108875c5e2c39937f18945d610
Luis Montes <monteslu@gmail.com>
2019-04-04 15:08:44 -0700

    formatting

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import { decode } from './qrclient';
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 class App extends Component {
   state = {
@@ -25,43 +26,48 @@ class App extends Component {
       const track = stream.getVideoTracks()[0];
       
       const videoElement = document.createElement('video');
-      const cameraCanvas = this.canvasRef.current; //document.createElement('canvas');
-      //const cameraCanvas = document.getElementById('canvas');
+      const drawCanvas = this.canvasRef.current; //document.createElement('canvas');
+      const decodeCanvas = document.createElement('canvas');
 
       videoElement.addEventListener('loadeddata', (e) => {
-        cameraCanvas.height = videoElement.videoHeight;
-        cameraCanvas.width = videoElement.videoWidth;
+        decodeCanvas.height = videoElement.videoHeight;
+        decodeCanvas.width = videoElement.videoWidth;
+        drawCanvas.height = videoElement.videoHeight;
+        drawCanvas.width = videoElement.videoWidth;
         videoElement.height = videoElement.videoHeight;
         videoElement.width = videoElement.videoWidth;
 
         console.log({video: {height: videoElement.height, width: videoElement.width}});
         
-        const ctx = cameraCanvas.getContext('2d');
-        ctx.font = "15px Arial";
-        ctx.fillStyle = "red";
+        const decodeCtx = decodeCanvas.getContext('2d');
+        const ctx = drawCanvas.getContext('2d');
         ctx.strokeStyle = "#FF0000";
-        ctx.lineWidth = 2;
+        ctx.lineWidth = 5;
 
         let times = [];
-
         
-        const onframe = async () => {
+        const onframe = () => {
           
-          ctx.drawImage(videoElement, 0, 0, videoElement.videoWidth, videoElement.videoHeight);
-          let endTime;
+          decodeCtx.drawImage(videoElement, 0, 0, videoElement.videoWidth, videoElement.videoHeight);
           const startTime = Date.now();
-          try {
-            
-            const bc = await decode(ctx, this.state.selectedType);
-            endTime = Date.now();
-            if(bc) {
-              this.setState({latestQR: bc});
-            }
+          let endTime;
+          let bc;
 
-            if(this.state.latestQR) {
-              
-              const cp = this.state.latestQR.cornerPoints;
-              ctx.fillText(this.state.latestQR.rawValue, cp[0].x, cp[0].y);
+          decode(decodeCtx, this.state.selectedType)
+          .then((result) => {
+            bc = result;
+            endTime = Date.now();
+            this.setState({currentQR: bc ? bc.rawValue: ''});
+          })
+          .catch((err) => {
+            endTime = Date.now();
+            console.log('err decoding', err);
+            this.setState({decodeError: err.message});
+          })
+          .then(async () => {
+            ctx.drawImage(decodeCanvas, 0, 0, videoElement.videoWidth, videoElement.videoHeight);
+            if(bc) {
+              const cp = bc.cornerPoints;
               ctx.beginPath();
               ctx.moveTo(cp[0].x, cp[0].y);
               ctx.lineTo(cp[1].x, cp[1].y);
@@ -70,22 +76,21 @@ class App extends Component {
               ctx.lineTo(cp[0].x, cp[0].y);
               ctx.closePath();
               ctx.stroke();
-              //console.log('a good bc', bc.cornerPoints);
+              
             }
-            this.setState({currentQR: bc ? bc.rawValue: ''});
+            
+            times.push({val: endTime - startTime, bc: bc ? 1 : 0});
+            if(times.length > 10) {
+              times.shift();
+            }
 
-            // console.log('elapsed', decodeTime, bc);
-          } catch (err) {
-            endTime = Date.now();
-            console.log('err decoding', err);
-          } 
-          times.push(endTime - startTime);
-          if(times.length > 10) {
-            times.shift();
-          }
-          const totalTime = times.reduce((acc, t) => acc + t, 0);
-          this.setState({avgTime: totalTime/times.length});
-          requestAnimationFrame(onframe);
+            const totalTime = times.reduce((acc, t) => acc + t.val, 0);
+            const totalHits = times.reduce((acc, t) => acc + t.bc, 0);
+            this.setState({avgTime: totalTime/times.length, hitPct: totalHits/times.length});
+            await sleep(100);
+            requestAnimationFrame(onframe);
+          });
+          
           
         };
 
@@ -110,14 +115,18 @@ class App extends Component {
   }
 
   render() {
+    const hitPct = this.state.hitPct || 0;
+    const hitColor = hitPct > 0.6 ? 'green' : hitPct < 0.4 ? 'red' : 'lightblue';
     return (
       <div className="App">
         <header className="App-header">
           <div style={{width: '95%', margin: '10px'}}>
-            <span style={{float: 'left'}}>QR Code found: {this.state.currentQR || ''}</span>
-            <span style={{float: 'right'}}>Avg. time(ms): {(this.state.avgTime || 0).toFixed(1)}</span>
+            <span style={{float: 'left'}}>Code: <span style={{color: 'lightblue'}}>{this.state.currentQR || ''}</span></span>
+            <span style={{}}>Hit: <span style={{color: hitColor}}>{Math.round(hitPct * 100)}%</span></span>
+            <span style={{float: 'right'}}>Avg. time(ms): <span style={{color: 'lightblue'}}>{(this.state.avgTime || 0).toFixed(1)}</span></span>
           </div>
-          <canvas ref={this.canvasRef}/>
+          <canvas ref={this.canvasRef} style={{transform: 'rotateY(180deg)'}}/>
+          <div style={{color: 'red', margin: '10px'}}>{this.state.decodeError}</div>
           <div className="custom02">
             <div>
               <input
@@ -126,7 +135,7 @@ class App extends Component {
                 value="js"
                 id="radio1"
                 checked={this.state.selectedType === "js"}
-                onChange={() => this.setState({selectedType: 'js'})}
+                onChange={() => this.setState({selectedType: 'js', decodeError: null})}
               />
               <label htmlFor="radio1">
                 JavaScript (jsqrcode)
@@ -140,7 +149,7 @@ class App extends Component {
                 value="js"
                 id="radio2"
                 checked={this.state.selectedType === "wasm"}
-                onChange={() => this.setState({selectedType: 'wasm'})}
+                onChange={() => this.setState({selectedType: 'wasm', decodeError: null})}
               />
               <label htmlFor="radio2">
                 WASM (zbar C++)
@@ -154,12 +163,13 @@ class App extends Component {
                 value="js"
                 id="radio3"
                 checked={this.state.selectedType === "native"}
-                onChange={() => this.setState({selectedType: 'native'})}
+                onChange={() => this.setState({selectedType: 'native', decodeError: null})}
               />
               <label htmlFor="radio3">
                 Native (BarcodeDetector)
               </label>
             </div>
+
           </div>
          
         </header>
